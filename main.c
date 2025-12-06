60ace63577e25195ef817fe5c9bc04bbe93951f3
struglyggwp <cheglakov198@gmail.com>
2025-12-06 16:23:57 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <windows.h>
+//#include "2z.c"
+
+
+
+// 1.1
 
 void check()
 {
@@ -8,32 +13,37 @@ void check()
     scanf_s("%u", &m);
     for (int i = (int)(sizeof(m)) * 8 - 1; i >= 0; i--)
     {
-        printf("%u", (m >> i) & 1u);
+        printf("%u", (m >> i) & 1);
     }
     printf("\n");
     return;
 }
+
+// 1.2
 
 void check_minus()
 {
-    int u;
+    int d;
     printf("Введите отрицательное целое число для представления его в двоичном виде\n");
-    scanf_s("%d", &u);
-    unsigned int m = (unsigned int)u;
+    scanf_s("%d", &d);
+    unsigned int m = (unsigned int)d;
     for (int i = (int)(sizeof(m)) * 8 - 1; i >= 0; i--)
     {
-        printf("%d", ((m >> i) & 1));
+        printf("%d", ((m >> i) & 1)); 
     }
+    
     printf("\n");
     return;
 }
 
+// 1.3
+
 void print_units() 
 {
-    int u;
+    int d;
     printf("Введите целое число для вывода кол-ва единиц в двоичном виде\n");
-    scanf_s("%d", &u);
-    unsigned int m = (unsigned int)u;
+    scanf_s("%d", &d);
+    unsigned int m = (unsigned int)d;
     int count = 0;
     for (int i = 0; i < (int)(sizeof(m) * 8); i++)
     {
@@ -42,9 +52,12 @@ void print_units()
             count++;
         }
     }
+  
     printf("%d\n", count);
     return;
 }
+
+// 1.4
 
 void change_third(int *m)
 {
@@ -53,6 +66,7 @@ void change_third(int *m)
     printf("Введите новое значение третьего байта в положительном целом числе\n");
     scanf_s("%u", &u);
     m1 = (unsigned int)*m;
+    printf("Изначальное значение числа в двоичной форме - ");
     for (int i = (int)(sizeof(m1)) * 8 - 1; i >= 0; i--)
     {
         printf("%u", (m1 >> i) & 1u);
@@ -61,9 +75,9 @@ void change_third(int *m)
     m1 = m1 & ~mask;
     m1 = m1 | (u << 16);
     *m = m1;
+    printf("Измененное значение числа в двоичной форме - ");
     for (int i = (int)(sizeof(*m)) * 8 - 1; i >= 0; i--)
     {
-    
         printf("%u", (*m >> i) & 1u);
     }
 }
@@ -75,12 +89,15 @@ int main(void)
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
-    check();              // Задание №1
-    check_minus();       // Задание №2
-    print_units();      // Задание №3
-    unsigned int m;    // Задание №4
-    printf("Введите значение целого положительного числа\n");  // Задание №4
-    scanf_s("%u", &m); // Задание №4
-    change_third(&m); // Задание №4
+    check();              // Задание 1.1
+    check_minus();        // Задание 1.2
+    print_units();        // Задание 1.3
+    unsigned int m;       // Задание 1.4
+    printf("Введите значение целого положительного числа\n");    // Задание 1.4
+    scanf_s("%u", &m);      // Задание 1.4
+    change_third(&m);       // Задание 1.4
+   // quad_m();             // Задание 2.1
+   // swap_rows();          // Задание 2.2
+   // trungle_m();          // Задание 2.3
+   // snail();              // Задание 2.4
 }
